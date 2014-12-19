b0c37f31ef49881817a67110d370b593f86387d4
Morketh <admmorketh@gmail.com>
2014-12-19 13:27:14 -0700

    Base Commit
    commit adds in all of the directories and files needed

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-uesfrp_techology
-================
+UESFRP Technology Code Section
+===============================
 
-This project is for UESFRP a place to hold all collaberation on programs and scripts
+Person Identification Key
+
+YYYYMMDDSOLPPSDEXB-CRC-ORG
+
+Discription of fields:
+
+SOL codes (3 digit system identifier)
+UMX = Unimatrix
+SOL = Solar System
+
+PP codes (planet number)
+Dual Digit
+
+
+SDEX codes
+can be found and calculated
+http://resources.rootsweb.ancestry.com/cgi-bin/soundexconverter
+
+
+B Code
+BirthNumber
+this number will take into account Twins or triplets
+
+
+CRC codes
+CRC-16  Males
+CRC-DNP Females
+CRC-16  CCITT ( Artificial Intelligence CRC )
+
+refrance values
+http://www.insidepro.com/hashes.php?lang=eng
+
+ORG codes (4 digit organization key)
+CRC Kermit codes
+
+
+## Examples ##
+
+Unimatrix 03 = U03
+BD = 1988-11-06
+Sorex = S620
+Subjunction 6 = (Kermit) DF89
+AI = CCITT
+
+Using the above information and the rules above should yeild the following ID tag
+
+YYYY = 1988
+MM = 11
+DD = 06
+SOL = UMX
+PP = 03
+SDEX = S620
+- CCITT - 
+ORG DF89
+
+19881106UMX03S6200 - CRC-16-CCITT - DF89
+
+CRC value can be calculated as such
+19881106UMX03S6200DF89 ---> 
+
+ID TAG
+19881106UMX03S6200--DF89
---
+++ soundex.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+##################################################################################################
+#
+# Copyright (C) 2014 Collective Industries
+#
+# AUTHOR: Andrew Malone
+#
+# TITLE: id-hash.py
+#
+# PURPOSE: servers as a back-end for the creation of ID hashes
+#
+# CREDITS: William Baggett for concept and original algorithm
+#
+##################################################################################################
+
+## Global Settings ##
+
+## Includes ##
+
+from CRCModules.CRC16 import CRC16
+from CRCModules.CRC16DNP import CRC16DNP
+from CRCModules.CRCCCITT import CRCCCITT
+from CRCModules.CRC16Kermit import CRC16Kermit
+
+## Functions ##
+
+def soundex(name, len=4):
+    """ soundex module conforming to Knuth's algorithm
+        implementation 2000-12-24 by Gregory Jorgensen
+        public domain
+    """
+
+    # digits holds the soundex values for the alphabet
+    digits = '01230120022455012623010202'
+    sndx = ''
+    fc = ''
+
+    # translate alpha chars in name to soundex digits
+    for c in name.upper():
+        if c.isalpha():
+            if not fc: fc = c   # remember first letter
+            d = digits[ord(c)-ord('A')]
+            # duplicate consecutive soundex digits are skipped
+            if not sndx or (d != sndx[-1]):
+                sndx += d
+
+    # replace first digit with first alpha character
+    sndx = fc + sndx[1:]
+
+    # remove all 0s from the soundex code
+    sndx = sndx.replace('0','')
+
+    # return soundex code padded to len characters
+    return (sndx + (len * '0'))[:len]
+
+## Main program ##
+if __name__ == "__main__":
+	input = "BannonInterplanetarySystems"
+	target = soundex(input)
+	print "Soundex of "+input+": " + target
+	print ("CRC-CCITT(XModem) {:10X}".format(CRCCCITT().calculate(target)))
+	print ("CRC-16            {:10X}".format(CRC16().calculate(target)))
+	print ("CRC-DNP           {:10X}".format(CRC16DNP().calculate(target)))
+	print ("CRC-16 (Kermit)   {:10X}".format(CRC16Kermit().calculate(target)))
+	
+	input = "Bannon debugging"
+	target = soundex(input)
+	print "Soundex of "+input+": " + target
+	print ("CRC-CCITT(XModem) {:10X}".format(CRCCCITT().calculate(target)))
+	print ("CRC-16            {:10X}".format(CRC16().calculate(target)))
+	print ("CRC-DNP           {:10X}".format(CRC16DNP().calculate(target)))
+	print ("CRC-16 (Kermit)   {:10X}".format(CRC16Kermit().calculate(target)))
